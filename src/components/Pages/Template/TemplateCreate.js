d0fc06d3d1fa2b03105936fa1d48d086c74eb47a
Tomas Jemelka <jemelkat.dev@gmail.com>
2021-12-11 00:34:21 +0100

    Added create and edit differences to template canvas

--- src/components/Pages/Template/TemplateCreate.js
+++ src/components/Pages/Template/TemplateCreate.js
@@ -1,17 +1,18 @@
 import Sidebar from "../../UI/Sidebar/Sidebar";
 import SidebarLinks from "../../UI/Sidebar/SidebarLinks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RndCanvasItem from "../../UI/CanvasItem/RndCanvasItem";
 import Button from "../../UI/Button";
 import * as Yup from "yup";
-import { Form, Formik } from "formik";
+import { Form, Formik, setNestedObjectValues } from "formik";
 import FormHidden from "../../UI/Form/FormHidden";
 import FormInput from "../../UI/Form/FormInput";
-import { axiosInstance } from "../../../helpers/AxiosHelper";
+import { axiosInstance, useAxios } from "../../../helpers/AxiosHelper";
+import { useHistory } from "react-router";
 
 const typeEnum = Object.freeze({
 	GRAPH: "GRAPH",
-	TEXT: "STATIC_TEXT",
+	STATIC_TEXT: "STATIC_TEXT",
 	TABLE: "TABLE",
 });
 
@@ -29,6 +30,15 @@ class Item {
 const TemplateCreate = (props) => {
 	//Stores all components currently on canvas
 	const [components, setComponents] = useState([]);
+	const history = useHistory();
+	const [{ data, loading, error }, fetchData] = useAxios(
+		{
+			url: "/template/get",
+			method: "GET",
+			params: { templateId: props.templateId },
+		},
+		{ useCache: false, manual: true }
+	);
 
 	//Get next id for new component
 	const nextItemId = () => {
@@ -50,7 +60,7 @@ const TemplateCreate = (props) => {
 		let item;
 		switch (type) {
 			case typeEnum.TEXT:
-				item = new Item(nextItemId(), 0, 0, 150, 50, typeEnum.TEXT);
+				item = new Item(nextItemId(), 0, 0, 150, 50, typeEnum.STATIC_TEXT);
 				break;
 			case typeEnum.GRAPH:
 				item = new Item(nextItemId(), 0, 0, 200, 200, typeEnum.GRAPH);
@@ -66,20 +76,21 @@ const TemplateCreate = (props) => {
 
 	//Saves template to DB
 	const saveTemplateHandler = (formValues) => {
-		let itemsArray = components;
-		if (props.mode === props.mode) {
-			itemsArray = components.map((e) => ({ ...e, itemId: null }));
-		}
 		axiosInstance
 			.post("/template/save", {
 				templateId: formValues.id,
 				templateName: formValues.templateName,
-				templateItems: itemsArray,
+				templateItems:
+					//TODO REMOVE LINE AFTER : - new items are created every time
+					props.mode === "create"
+						? components.map((e) => ({ ...e, itemId: null }))
+						: components.map((e) => ({ ...e, itemId: null })),
 			})
 			.then(function (response) {
-				console.log(response);
+				history.push("/template");
 			})
 			.catch(function (error) {
+				//TODO ADD ALERT
 				console.log(error);
 			});
 	};
@@ -92,90 +103,109 @@ const TemplateCreate = (props) => {
 		);
 	};
 
+	useEffect(() => {
+		if (props.mode === "edit") fetchData();
+	}, []);
+
+	useEffect(() => {
+		if (data) {
+			const newComponents = data.templateItems.map(
+				(i) => new Item(i.itemId, i.x, i.y, i.width, i.height, i.type)
+			);
+			setComponents(newComponents);
+		}
+	}, [data]);
+
 	return (
 		<>
-			<div className='flex'>
-				{/*Left sidebar */}
-				<div className='flex-1 mr-2 xl:mr-4'>
-					<div className='flex justify-start h-screen sticky top-0'>
-						<Sidebar className='bg-gray-300 overflow-y-auto'>
-							<SidebarLinks sidebarName='Report'>
-								<Formik
-									initialValues={{
-										id: props.templateId === null ? null : props.templateId,
-										templateName: "",
-									}}
-									validationSchema={Yup.object({
-										templateName: Yup.string().required("Required"),
-									})}
-									onSubmit={(values, { setSubmitting }) => {
-										saveTemplateHandler(values);
-										setSubmitting(false);
-									}}
+			{loading && props.mode === "edit" ? (
+				<div>LOADING...</div>
+			) : (
+				<div className='flex'>
+					{/*Left sidebar */}
+					<div className='flex-1 mr-2 xl:mr-4'>
+						<div className='flex justify-start h-screen sticky top-0'>
+							<Sidebar className='bg-gray-300 overflow-y-auto'>
+								<SidebarLinks sidebarName='Report'>
+									<Formik
+										initialValues={{
+											id: data ? data.templateId : null,
+											templateName: data ? data.templateName : "",
+										}}
+										validationSchema={Yup.object({
+											templateName: Yup.string().required("Required"),
+										})}
+										onSubmit={(values, { setSubmitting }) => {
+											saveTemplateHandler(values);
+											setSubmitting(false);
+										}}
+									>
+										{({ handleChange, values }) => (
+											<Form className='flex flex-col p-4'>
+												<FormHidden name='id'></FormHidden>
+												<FormInput
+													label='Template name'
+													name='templateName'
+													type='text'
+													placeholder='Template name...'
+												/>
+												<Button type='submit' className='mt-4'>
+													Save
+												</Button>
+											</Form>
+										)}
+									</Formik>
+								</SidebarLinks>
+								<SidebarLinks sidebarName='Template components'></SidebarLinks>
+								<div
+									className='bg-gray-200 p-2 m-2'
+									onClick={() => addComponentHandler(typeEnum.TEXT)}
 								>
-									<Form className='flex flex-col p-4'>
-										<FormHidden name='id'></FormHidden>
-										<FormInput
-											label='Template name'
-											name='templateName'
-											type='text'
-											placeholder='Template name...'
-										/>
-										<Button type='submit' className='mt-4'>
-											Save
-										</Button>
-									</Form>
-								</Formik>
-							</SidebarLinks>
-							<SidebarLinks sidebarName='Template components'></SidebarLinks>
-							<div
-								className='bg-gray-200 p-2 m-2'
-								onClick={() => addComponentHandler(typeEnum.TEXT)}
-							>
-								TEXT
-							</div>
-							<div
-								className='bg-gray-200 p-2 m-2'
-								onClick={() => addComponentHandler(typeEnum.GRAPH)}
-							>
-								GRAPH
-							</div>
-							<div
-								className='bg-gray-200 p-2 m-2'
-								onClick={() => addComponentHandler(typeEnum.TABLE)}
-							>
-								TABLE
-							</div>
-						</Sidebar>
+									TEXT
+								</div>
+								<div
+									className='bg-gray-200 p-2 m-2'
+									onClick={() => addComponentHandler(typeEnum.GRAPH)}
+								>
+									GRAPH
+								</div>
+								<div
+									className='bg-gray-200 p-2 m-2'
+									onClick={() => addComponentHandler(typeEnum.TABLE)}
+								>
+									TABLE
+								</div>
+							</Sidebar>
+						</div>
+					</div>
+					{/*Canvas*/}
+					<div className='mt-10 mb-10 overflow-x-auto overflow-y-hidden'>
+						<div
+							className='border-2 shadow-xl relative m-auto'
+							style={{ width: "210mm", height: "297mm" }}
+						>
+							{/*Generate stored components to canvas */}
+							{components.map((i) => {
+								return (
+									<RndCanvasItem
+										key={i.itemId}
+										item={i}
+										onMove={moveItemHandler}
+									></RndCanvasItem>
+								);
+							})}
+						</div>
+					</div>
+					{/*Right sidebar */}
+					<div className='flex-1 ml-2 xl:ml-4'>
+						<div className='flex justify-end h-screen sticky top-0'>
+							<Sidebar className='bg-gray-300'>
+								<SidebarLinks sidebarName='Edit selected component'></SidebarLinks>
+							</Sidebar>
+						</div>
 					</div>
 				</div>
-				{/*Canvas*/}
-				<div className='mt-10 mb-10 overflow-x-auto overflow-y-hidden'>
-					<div
-						className='border-2 shadow-xl relative m-auto'
-						style={{ width: "210mm", height: "297mm" }}
-					>
-						{/*Generate stored components to canvas */}
-						{components.map((i) => {
-							return (
-								<RndCanvasItem
-									key={i.itemId}
-									item={i}
-									onMove={moveItemHandler}
-								></RndCanvasItem>
-							);
-						})}
-					</div>
-				</div>
-				{/*Right sidebar */}
-				<div className='flex-1 ml-2 xl:ml-4'>
-					<div className='flex justify-end h-screen sticky top-0'>
-						<Sidebar className='bg-gray-300'>
-							<SidebarLinks sidebarName='Edit selected component'></SidebarLinks>
-						</Sidebar>
-					</div>
-				</div>
-			</div>
+			)}
 		</>
 	);
 };

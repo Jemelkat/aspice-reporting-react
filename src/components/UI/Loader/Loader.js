96ed7ced662c7d805f60bbe8cfcfd3436cd2d190
Tomas Jemelka <jemelkat.dev@gmail.com>
2022-02-05 14:05:42 +0100

    Added progress status for source upload

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -1,27 +1,30 @@
-const Loader = ({ fullscreen = true, dark = true }) => {
+const Loader = ({ fullscreen, dark, size = "normal", ...props }) => {
 	return (
-		<div
-			className={`${
-				fullscreen === true ? "min-h-screen min-w-full" : "h-full"
-			} ${
-				dark === true ? "bg-gray-800" : "bg-white"
-			} flex flex-1 justify-center items-center space-x-3`}
-		>
+		<div className='justify-center text-center'>
 			<div
-				className={`${
-					dark === true ? "bg-white" : "bg-gray-800"
-				} w-8 h-8  rounded-full animate-bounce`}
-			></div>
-			<div
-				className={`${
-					dark === true ? "bg-white" : "bg-gray-800"
-				} w-8 h-8 rounded-full animate-bounce animation-delay-500`}
-			></div>
-			<div
-				className={`${
-					dark === true ? "bg-white" : "bg-gray-800"
-				} w-8 h-8  rounded-full animate-bounce`}
-			></div>
+				className={`${fullscreen ? "min-h-screen min-w-full" : "h-full"} ${
+					dark ? "bg-gray-800" : "bg-white"
+				} flex justify-center items-center space-x-3`}
+			>
+				<div
+					className={`${dark ? "bg-white" : "bg-gray-800"}  ${
+						size === "small" ? "w-4 h-4 " : "w-8 h-8 "
+					} rounded-full animate-bounce`}
+				></div>
+				<div
+					className={`${dark ? "bg-white" : "bg-gray-800"} ${
+						size === "small" ? "w-4 h-4 " : "w-8 h-8 "
+					} rounded-full animate-bounce animation-delay-500`}
+				></div>
+				<div
+					className={`${dark ? "bg-white" : "bg-gray-800"} ${
+						size === "small" ? "w-4 h-4 " : "w-8 h-8 "
+					}  rounded-full animate-bounce`}
+				></div>
+			</div>
+			<p className={`${size === "small" ? "text-sm" : "text-base"}`}>
+				{props.children}
+			</p>
 		</div>
 	);
 };

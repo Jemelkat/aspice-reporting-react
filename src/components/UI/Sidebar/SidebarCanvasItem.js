dbb6073557238cd2c889d5fa3977017fa67d2a49
Tomas Jemelka <JEM0021@vsb.cz>
2022-02-14 02:36:53 +0100

    Created sidebar canvas clickable item

--- src/components/UI/Sidebar/SidebarCanvasItem.js
+++ src/components/UI/Sidebar/SidebarCanvasItem.js
@@ -1,13 +1,11 @@
 const SidebarCanvasItem = (props) => {
 	return (
 		<div
-			className='items-center justify-center mb-4 cursor-pointer w-52 hover:bg-gray-200 '
+			className='items-center justify-center w-48 mb-4 shadow-md cursor-pointer hover:bg-gray-200'
 			onClick={props.onClick}
 		>
 			<div className='h-6 pl-2 text-white bg-gray-800'>{props.name}</div>
-			<div className='border border-gray-800 shadow-lg h-28'>
-				{props.children}
-			</div>
+			<div className='h-24 border border-gray-800 '>{props.children}</div>
 		</div>
 	);
 };

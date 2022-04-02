cd09d442ea095243a853b0e3e86c21a5a856fc47
Tomáš Jemelka <jemelkat.dev@gmail.com>
2022-04-02 17:59:49 +0200

    Added pages paggination to report canvas

--- src/components/Report/ReportCreate.js
+++ src/components/Report/ReportCreate.js
@@ -12,13 +12,16 @@ import PDFPreview from "../Canvas/PDFPreview";
 import { saveAs } from "file-saver";
 import ReportService from "../../services/ReportService";
 import { createItemFromExisting } from "../../helpers/ClassHelper";
+import ReactPaginate from "react-paginate";
+import { MinusIcon, PlusIcon } from "@heroicons/react/outline";
 
 const ReportCreate = ({ mode, reportId, addItem = null }) => {
 	const [reportData, setReportData] = useState(null);
 	const [reportLoading, setReportLoading] = useState(true);
 	const [previewData, setPreviewData] = useState(null);
 	const [isProcessing, setProcessing] = useState(false);
-
+	const [currentPage, setCurrentPage] = useState(0);
+	const [numberOfPages, setNumberOfPages] = useState(1);
 	const alert = useAlert();
 	let history = useHistory();
 	const {
@@ -177,6 +180,24 @@ const ReportCreate = ({ mode, reportId, addItem = null }) => {
 		}
 	}, []);
 
+	const handlePageClick = (event) => {
+		setCurrentPage(event.selected);
+	};
+
+	const addPageHandler = () => {
+		setNumberOfPages(numberOfPages + 1);
+	};
+
+	const removePageHandler = () => {
+		const newNumberOfPages = numberOfPages - 1;
+		if (numberOfPages > 1) {
+			setNumberOfPages(newNumberOfPages);
+			if (currentPage >= newNumberOfPages) {
+				setCurrentPage(newNumberOfPages - 1);
+			}
+		}
+	};
+
 	return (
 		<>
 			{reportLoading && mode === "edit" ? (
@@ -221,7 +242,7 @@ const ReportCreate = ({ mode, reportId, addItem = null }) => {
 								<Tab
 									className={({ selected }) => {
 										return (
-											"p-2 w-20 rounded-tr-lg rounded-br-lg " +
+											"p-2 w-20 rounded-tr-lg rounded-br-lg  " +
 											(selected
 												? "bg-gray-800 text-white font-bold"
 												: "bg-white text-gray-800")
@@ -233,6 +254,44 @@ const ReportCreate = ({ mode, reportId, addItem = null }) => {
 							</Tab.List>
 							<Tab.Panels>
 								<Tab.Panel>
+									<div className='flex flex-col items-center justify-center pt-2'>
+										<ReactPaginate
+											forcePage={currentPage}
+											nextLabel={">"}
+											onPageChange={handlePageClick}
+											pageRangeDisplayed={2}
+											marginPagesDisplayed={1}
+											pageCount={numberOfPages}
+											previousLabel={"<"}
+											pageClassName='border text-lg border-gray-800 w-8 text-center h-8 rounded-lg'
+											pageLinkClassName=''
+											previousClassName=' border border-gray-800 bg-gray-800 text-white rounded-lg text-lg  text-center h-8 w-8 mr-2'
+											previousLinkClassName=''
+											nextClassName='border border-gray-800 bg-gray-800 text-white text-lg rounded-lg text-center h-8 w-8 ml-2'
+											nextLinkClassName=''
+											breakLabel='...'
+											breakClassName='pl-2 pr-2'
+											breakLinkClassName=''
+											containerClassName='flex items-center justify-center'
+											activeClassName='bg-gray-800 text-white border-gray-800'
+											renderOnZeroPageCount={null}
+										/>
+										<div className='flex items-center' onClick={addPageHandler}>
+											<span className='flex items-center justify-center w-8 h-8 bg-gray-800 rounded-lg cursor-pointer'>
+												<PlusIcon className='w-5 h-5 text-white'></PlusIcon>
+											</span>
+											<span className='pl-2'>Add page</span>
+										</div>
+										<div
+											className='flex items-center'
+											onClick={removePageHandler}
+										>
+											<span className='flex items-center justify-center w-8 h-8 bg-gray-800 rounded-lg cursor-pointer'>
+												<MinusIcon className='w-5 h-5 text-white'></MinusIcon>
+											</span>
+											<span className='pl-2'>Remove page</span>
+										</div>
+									</div>
 									{/*Canvas */}
 									<Canvas
 										items={items}

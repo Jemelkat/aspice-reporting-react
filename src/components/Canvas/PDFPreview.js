264a3cf0d6b7552f78f5df9dee6ea839a4d72d29
Tomáš Jemelka <jemelkat.dev@gmail.com>
2022-04-25 02:18:21 +0200

    Fixed report PDF visualisation

--- src/components/Canvas/PDFPreview.js
+++ src/components/Canvas/PDFPreview.js
@@ -1,18 +1,16 @@
 const PDFPreview = ({ pdfData }) => {
 	return (
-		<div className='flex flex-col items-center justify-center'>
-			<div
-				className='flex justify-center overflow-x-auto overflow-y-auto bg-white'
-				style={{ width: "210mm", height: "297mm" }}
-			>
-				{pdfData ? (
-					<iframe title='preview' className='flex-1' src={pdfData}></iframe>
-				) : (
-					<div className='pt-4 text-center align-middle'>
-						Please generate the report first.
-					</div>
-				)}
-			</div>
+		<div
+			className='flex justify-center mt-2 bg-white'
+			style={{ width: "210mm", height: "297mm" }}
+		>
+			{pdfData ? (
+				<iframe title='preview' className='w-full' src={pdfData}></iframe>
+			) : (
+				<div className='pt-4 text-center align-middle'>
+					Please generate the report first.
+				</div>
+			)}
 		</div>
 	);
 };
